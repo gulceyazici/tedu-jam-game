4b43d79ed6603372ee0138efc8866a7cbb47f0b6
harunugurlu <harunugurlu1@gmail.com>
2024-03-10 00:06:50 +0300

    add particles player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,16 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float playerShipSpeed;
+
+    [SerializeField]
+    private float playerShipXSpeed;
     [SerializeField]
     private float rotationSpeed;
-    
+    [SerializeField] private AudioSource thrusterAudio;
+    [SerializeField] private ParticleSystem thrusterParticlesLeft;
+    [SerializeField] private ParticleSystem thrusterParticlesRight;
+
+
     public float verticalInput;
     public float horizontalInput;
 
@@ -18,15 +25,18 @@ public class PlayerController : MonoBehaviour
 
     private float maxRotateAngle = 45.0f;
 
-    private float xRange = 100.0f;
+    private float xRange = 300.0f;
 
     private float budget = 0f;
-
 
     // Start is called before the first frame update
     void Start()
     {
-
+    }
+    
+    private void Update()
+    {
+        PlaySoundEffects();
     }
 
     // Update is called once per frame
@@ -34,11 +44,13 @@ public class PlayerController : MonoBehaviour
     {
         MoveShip();
         RotateShip();
+
     }
 
     private void MoveShip()
     {
         verticalInput = UnityEngine.Input.GetAxis("Vertical");
+
 
         GetComponent<Rigidbody>().AddForce(verticalInput * playerShipSpeed * Time.deltaTime *Vector3.up , ForceMode.Impulse);
 
@@ -50,6 +62,7 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+ 
     }
 
     private void RotateShip()
@@ -81,9 +94,41 @@ public class PlayerController : MonoBehaviour
         {
             GetComponent<Rigidbody>().AddForce(1 * playerShipSpeed * Time.deltaTime* Vector3.right, ForceMode.Impulse);
         }
-        GetComponent<Rigidbody>().AddForce(horizontalInput * playerShipSpeed * Time.deltaTime* Vector3.right  , ForceMode.Impulse);
+        GetComponent<Rigidbody>().AddForce(horizontalInput * playerShipXSpeed * Time.deltaTime* Vector3.right  , ForceMode.Impulse);
     }
 
+    private void PlaySoundEffects()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            // Check if thrusterAudio is assigned and not null
+            if (thrusterAudio != null && thrusterAudio.isPlaying == false)
+            {
+                // Play the audio
+                thrusterAudio.Play();
+                thrusterParticlesLeft.Play();
+                thrusterParticlesRight.Play();
+
+            }
+            else
+            {
+                Debug.LogWarning("thrusterAudio is not assigned!");
+            }
+        }
+
+        // Check if the W key is released
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            // Check if thrusterAudio is playing
+            if (thrusterAudio != null && thrusterAudio.isPlaying)
+            {
+                // Stop the audio
+                thrusterAudio.Stop();
+                thrusterParticlesLeft.Stop();
+                thrusterParticlesRight.Stop();
+            }
+        }
+    }
     public float GetBudget()
     {
         return this.budget;
@@ -93,5 +138,4 @@ public class PlayerController : MonoBehaviour
     {
         this.budget = money;
     }
-
 }
